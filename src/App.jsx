aef5711dc51c1f2b1304d208d6b5513de654e94d
david-code-portfolio <ksendzulak.biz@gmail.com>
2025-05-20 20:24:31 +0200

    Style config | First elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './index.scss'
+import './components/MainBtn'
+import MainBtn from './components/MainBtn'
 
 function App(){
   return <>
-    <h1 class="text-8xl">TEXT</h1>
-    <p class="text-2xl italic">hello</p>
+    <h5 className='uppercase font-bold text-[16px] tracking-[0.25em] absolute left-[80px] top-[80px] select-none'>CREATED BY dávid kšendzuľák | 2025</h5>
+    <h1 className="text-[300px]/[300px] font-medium mt-[180px] ml-[60px] cursor-default select-none">FOLDER.</h1>
+    <h1 className='absolute text-[600px]/[600px] top-[95px] left-0 background_text select-none'>FOLDER</h1>
+    <MainBtn></MainBtn>
   </>
 }
 export default App
