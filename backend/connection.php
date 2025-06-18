e33fd4313651c3ba2fa4f377cf80f678b88014a2
david-code-portfolio <ksendzulak.biz@gmail.com>
2025-06-18 15:12:12 +0200

    Remove back-end from tracking

--- backend/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "folder database";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-?>
